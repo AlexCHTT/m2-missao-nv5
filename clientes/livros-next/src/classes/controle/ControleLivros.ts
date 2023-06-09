54ca60a8122cd5105b1d2f5609cbc955b50d4553
AlexCHTT <116036494+AlexCHTT@users.noreply.github.com>
2023-06-09 20:17:00 -0300

    Add files via upload

---
+++ clientes/livros-next/src/classes/controle/ControleLivros.ts
@@ -0,0 +1,51 @@
+import Livro from "../modelo/Livros"
+
+
+var livros: Array<Livro> = [
+    {
+      codigo: 1,
+      codEditora: 1,
+      titulo: 'O poder do hábito',
+      resumo: 'Durante os últimos dois anos, uma jovem transformou quase todos os aspectos de sua vida.',
+      autores: [' Charles Duhigg', ' Rafael Mantovani']
+    },
+    {
+      codigo: 2,
+      codEditora: 2,
+      titulo: 'Do Mil ao Milhão',
+      resumo: 'Criador da plataforma O Primo Rico, ensina aos leitores os três pilares para atingir a independência financeira',
+      autores: ['Thiago Nigro']
+    },
+    {
+      codigo: 3,
+      codEditora: 3,
+      titulo: 'O Menino Maluquinho',
+      resumo: 'Um menininho traquinas, diziam. Tinha macaquinhos no sótão, deitava e rolava, fazendo confusão.',
+      autores: ['Ziraldo Alves Pinto', 'Ziraldo']
+    },
+  ]
+
+  export default class ControleLivro {
+    livros = livros
+  
+    obterLivros() {
+      return livros
+    }
+    incluir(livro: Livro) {
+      try {
+        var codigo: number = (this.livros.length - 1);
+        livro.codigo = this.livros[codigo].codigo + 1;
+        this.livros.push(livro);
+      }
+      catch {
+        livro.codigo = 1;
+        this.livros.push(livro);
+      }
+    }
+    excluir(num: Number) {
+      var livroEncontrado = this.livros.findIndex((livro: Livro) => {
+        return num === livro.codigo
+      });
+      this.livros.splice(livroEncontrado, 1)
+    }
+  }
